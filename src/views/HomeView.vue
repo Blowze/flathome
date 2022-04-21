2ce47a1d250bd534d30bed1d36b8dae9e7404903
Aleksey Kraynov <aleksey.Kraynov@acronis.com>
2022-04-22 01:39:29 +0300

    fix theme

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,61 +1,15 @@
 <template>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
+  <div class="app-loader">
 
+  </div>
+  <div class="home">
+  <div class="home__button" @click="window.Telegram.WebApp.expand()">sdf</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'HomeView',
-   data() {
-            return {
-                    options: [
-      'Москва',
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Нижний Новгород',
-
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Нижний Новгород',
-      'Казань'
-    ]
-            }
-        },
-
    methods: {
     dropdownShouldOpen(VueSelect) {
       console.log()
@@ -70,6 +24,10 @@ div {
   width: 150px;
   margin-bottom: 20px;
   background: #ccc;
+}
+
+.home {
+  padding: 0 16px;
 }
 
 </style>
