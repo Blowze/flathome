d6de130041212d623be8c9b628c2b33bab6e87ae
Aleksey Kraynov <aleksey.Kraynov@acronis.com>
2022-04-22 18:36:39 +0300

    fix themes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import { createApp } from "vue";
 import Skeleton from "vue-loading-skeleton";
-import Vue3TouchEvents from "vue3-touch-events";
 import App from "./App.vue";
 import router from "./router";
 import "vue-loading-skeleton/dist/style.css";
 
-createApp(App).use(router).use(Skeleton).use(Vue3TouchEvents).mount("#app");
+createApp(App).use(router).use(Skeleton).mount("#app");
