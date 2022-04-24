4e7433057480be4277a398ea78bc3437714f3ac6
Aleksey Kraynov <aleksey.Kraynov@acronis.com>
2022-04-24 14:24:11 +0300

    refactoring: aligned  files and deleted unnecessary comments

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "../views/CityView.vue";
 import RegionView from "../views/RegionView.vue";
 import FilterView from "../views/FilterView.vue";
 
@@ -12,9 +12,6 @@ const routes = [
     {
         path: "/filter",
         name: "about",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: FilterView,
     },
     {
