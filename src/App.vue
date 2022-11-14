edb0fbdd17e5ef9fd688f2fc09ec16d08607bae9
Apte4ka112 <krainoff.alesha@yandex.ru>
2022-11-14 11:14:29 +0300

    Убрал ошибку

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,8 @@ export default {
     name: "HomeView",
     mounted() {
         this.$nextTick(() => {
-            Telegram.WebApp.ready();
-            Telegram.WebApp.MainButton.onClick(Cafe.mainBtnClicked);
+            // Telegram.WebApp.ready();
+            // Telegram.WebApp.MainButton.onClick(Cafe.mainBtnClicked);
         });
     },
 };
@@ -39,7 +39,7 @@ html {
     min-height: 100%;
     &__container {
         width: 100%;
-        max-width: 560px;
+        max-width: 480px;
         margin: 0 auto;
         padding: var(--space-small) var(--space-normal);
         text-align: center;
